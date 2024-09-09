cdc80fc077e8425f6908333bc2b3970741b53b63
Ben Maves <benmaves@Bens-MacBook-Pro.local>
2024-09-09 15:47:34 -0400

    clean up

--- cow_fun.py
+++ cow_fun.py
@@ -25,35 +25,27 @@ def cow_energy(v, pos):
 	V = m * 9.8 * pos[1]
 	return T,V
 
-v = np.array([50,50])
-pos = np.array([0, 200])
+v = np.array([5,5])
+pos = np.array([0, 20])
 step = 0.1
 m = 1000
 
 x = []
 y = []
-t = []
 T = []
 V = []
-for i in range(50):
-	if pos[1] <= 0:
-		t.append(step*i)
-		x.append(pos[0])
-		y.append(pos[1])
-		T.append(0)
-		V.append(0)
-	else:
-		t.append(step*i)
-		force = cow_force(v, pos)
-		k, pot = cow_energy(v, pos)
-		v, pos = cow_update(v, pos, force, step)
-		x.append(pos[0])
-		y.append(pos[1])
-		T.append(k)
-		V.append(pot)
+while (pos[1] >= 0):
+	force = cow_force(v, pos)
+	k, pot = cow_energy(v, pos)
+	v, pos = cow_update(v, pos, force, step)
+	x.append(pos[0])
+	y.append(pos[1])
+	T.append(k)
+	V.append(pot)
+t = np.arange(len(x))*step
 plt.plot(x,y)	
-plt.savefig('cow_fig.png')	
+plt.savefig('cow_figures/cow_fig.png')	
 plt.clf()
 plt.plot(t, T)
 plt.plot(t,V)
-plt.savefig('cow_fid2.png')
+plt.savefig('cow_figures/cow_fid2.png')
