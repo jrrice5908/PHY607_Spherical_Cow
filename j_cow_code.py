715cc52b66ba8f7e7ce2a04799051c80b8aedd64
jrricesyr <jrrice@syr.edu>
2024-09-09 15:44:23 -0400

    small additions to Julia's code, still a WIP

---
+++ j_cow_code.py
@@ -0,0 +1,32 @@
+"""
+
+"""
+
+
+###Defining Initial Position Conditions###
+x0 = 0 #init x position is at origin
+y0 = h #init y position defined as some arbitrary height
+t0 = 0 #init time set to 0
+
+###Defining Initial Velocity Conditions###
+vx0 = 
+vy0 = 
+
+###Defining vectors that describe initial conditions###
+r0 = [x0, y0] 
+v0 = [vx0, vy0] 
+
+###Defining Dynamical Relationships###
+time_step = 1 #arbitrarily saying this = 1 second for now
+t = t0 + time_step*
+
+x = x0 + vx0*t
+y = y0 + vy0*t
+
+vx = vx0 - time_step*t
+vy = vy0 - time_step*t
+
+
+
+#basic script that can run indicated example, dont necesarily need to have questions answered
+
