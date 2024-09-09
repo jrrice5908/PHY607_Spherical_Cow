fcda8d81fe57ecd82bb6ed4c7e94e056182d8ba2
jrricesyr <jrrice@syr.edu>
2024-09-09 17:39:08 -0400

    added functions and whatnot

--- j_cow_code.py
+++ j_cow_code.py
@@ -1,49 +1,48 @@
-"""
-
-"""
-
 import math
 
-###Defining Constants###
-g = -9.81 #gravitational constant
-k = -1 #wind resistance constant
-m = 1000 #mass of cow
-dt = 1 #arbitrary time step, in seconds
-
-###Defining Initial Position Conditions###
-x0 = 0 #init x position is at origin
-y0 = h #init y position defined as some arbitrary height
-t0 = 0 #init time set to 0
-
-###Defining Initial Velocity Conditions###
-vx0 = 3 #arbitrary
-vy0 = 4 #arbitrary
-
-###Defining vectors that describe initial conditions###
-r0 = math.sqrt(((x0)^2)+((y0)^2))
-v0 = math.sqrt(((vx0)^2)+((vy0)^2))
-
-###Defining Functions###
-xt = x0 + vx*t #x after dt
-yt = y0 + vy*t #y after dt
-j_
-vxt = vx0 + xt/t
-vyt = vy0 + yt/t
-
-
-r = math.sqrt(((xt)^2)+((yt)^2))
-v = math.sqrt(((vxt)^2)+((vyt)^2))
+################################# Functions ################################# 
 
 def total_force_vector(r,v):
-    Fgrav = m*g
-    Fdrag = k*(v^2)
-    tot_F = Fgrav+Fdrag
+    Fgrav = [0,-g]
+    Fdrag = [-k*(v[0]), -k*(v[1])]
+    tot_F = [Fgrav[0]+Fdrag[0],Fgrav[1]+Fdrag[1]]
     return tot_F
 
-def v_and_r_after_small_time(r,v,tot_F,time_step)
+def r_and_v_after_dt(r,v,tot_F, dt):
+    ax = tot_F[0]/m
+    ay = tot_F[1]/m
+
+    vxnew = v[0] + ax*dt
+    vynew = v[1] + ay*dt
+    vnew = [vxnew, vynew]
+
+    xnew = r[0] + v[0]*dt + (1/2)*ax*(dt^2)
+    ynew = r[1] + v[1]*dt + (1/2)*ay*(dt^2)
+    rnew = [xnew, ynew]
     
+    return vnew, rnew
+
+def energies(r,v)
+    T = (1/2)*m*((v[0])^2+ (v[1])^2)
+    V = m*g*r[1]
+
+    return T,V
+
+################################# Constants and initial input #################################
+
+g = -9.81   #gravitational constant
+k = -1      #wind resistance constant
+m = 1000    #mass of cow
+dt = 1      #arbitrary time step, in seconds
+
+r = [x,y]       #arbitrary initial coords 
+#   [0],[1]
+v = [vx,vy]     #arbitrary initial vel
+
+################################# Main #################################
 
 
 
-# for tuesday, have basic script that can run indicated example, dont necesarily need to have questions answered
+
+
 
